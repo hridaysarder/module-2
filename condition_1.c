d45742bb76fdb5aacfbf0866f6c9e6338aed851c
HridaySarder <hridoyaug12@gmail.com>
2024-03-31 22:52:24 +0600

    added condition-1

---
+++ condition_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d",&tk);
+    if(tk>=100)
+    {
+        printf("Burgur khabo");
+    }
+    else
+    {
+        printf("Ajke saradin kicu khabo na");
+    }
+    return 0;
+}
